aad23b6ff4a86492f8cdf23d7a0229e626aff723
Khoding <julien.rutscho@ceff.ch>
2025-07-26 23:58:13 +0200

    fix and update, information text

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -15,6 +15,14 @@
 
       <button type="submit">Login</button>
 
+      <p>
+        <span class="bold">INFORMATION&nbsp;:</span> This is a modified
+        <span class="bold">demo version</span> of the project I worked on at Uni-Freiburg's
+        Rechenzentrum. It doesn't represent perfectly the job I did while I was there and a few
+        things are broken. It is for example lacking the data saving since all the data was
+        localised into a JSON file.
+      </p>
+
       <p v-if="error" class="error-message">{{ error }}</p>
     </form>
   </div>
@@ -28,8 +36,8 @@ import {useAuthStore} from '@/stores/auth-store';
 const router = useRouter();
 const authStore = useAuthStore();
 
-const username = ref('');
-const password = ref('');
+const username = ref('test@test.de');
+const password = ref('123456');
 const error = ref('');
 
 const login = () => {
